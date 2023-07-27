6633b59b7db0b62093fb44126ff1f950f8262987
Islam Ayman <islam.ayman.mohammed@gmail.com>
2023-07-27 17:24:59 +0300

    :sparkles: Enable `django-debug-toolbar` (#6)

--- config/development.py
+++ config/development.py
@@ -1,1 +1,14 @@
+import socket
+
 from .base import *  # noqa
+
+# django-debug-toolbar:
+# A configurable set of panels that display various debug information about the current
+# request/response.
+INSTALLED_APPS.append("debug_toolbar")
+MIDDLEWARE.insert(0, "debug_toolbar.middleware.DebugToolbarMiddleware")
+DEBUG_TOOLBAR_CONFIG = {"JQUERY_URL": ""}
+INTERNAL_IPS = [
+    "127.0.0.1",
+    socket.gethostbyname(socket.gethostname())[:-1] + "1",
+]
