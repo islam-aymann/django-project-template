d8d2a22634ce27241c80923cec21be9090dc0db9
Islam Ayman <islam.ayman.mohammed@gmail.com>
2023-07-27 17:08:01 +0300

    ✨ Add dynamic site config example (#5)
    * :sparkles: Add basic model entities
    
    * :sparkles: Add django-solo & django-jsonform site configuration example

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,55 +1,29 @@
 from django.contrib import admin
 from django.utils.translation import gettext_lazy as _
+from solo.admin import SingletonModelAdmin
 
-from apps.core.models import Tag, Post
+from apps.core.models import Tag, Post, SiteConfiguration
 
 admin.site.site_header = _("Project Name")
 admin.site.site_title = _("Project Name")
 
 
+@admin.register(SiteConfiguration)
+class SiteConfigurationAdmin(SingletonModelAdmin):
+    list_display = ["id", "name", "description", "maintenance_mode", "tag_colors"]
+
+
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
-    list_display = [
-        "id",
-        "name",
-    ]
-
-    search_fields = [
-        "name",
-    ]
+    list_display = ["id", "name"]
+    search_fields = ["name"]
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_select_related = [
-        "author",
-    ]
-
-    raw_id_fields = [
-        "author",
-    ]
-
-    list_display = [
-        "id",
-        "title",
-        "status",
-        "author",
-        "created",
-    ]
-
-    list_filter = [
-        "title",
-        "status",
-        "author",
-        "created",
-    ]
-
-    search_fields = [
-        "title",
-        "content",
-        "tags__name",
-    ]
-
-    filter_horizontal = [
-        "tags",
-    ]
+    list_select_related = ["author"]
+    raw_id_fields = ["author"]
+    list_display = ["id", "title", "status", "author", "created"]
+    list_filter = ["title", "status", "author", "created"]
+    search_fields = ["title", "content", "tags__name"]
+    filter_horizontal = ["tags"]
